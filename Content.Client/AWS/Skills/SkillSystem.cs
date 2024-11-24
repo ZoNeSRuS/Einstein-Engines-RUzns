573b5089a0dc64c1cfbbdd293c9992255082c624
aw-c <shitikov_1910@mail.ru>
2024-11-24 21:07:56 +0300

    pupupu

---
+++ Content.Client/AWS/Skills/SkillSystem.cs
@@ -0,0 +1,11 @@
+using Content.Shared.AWS.Skills;
+
+namespace Content.Client.AWS.Skills;
+
+public sealed class SkillSystem : SharedSkillSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
