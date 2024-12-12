96776604880d710dd6fb3299128c5791eb382732
aw-c <shitikov_1910@mail.ru>
2024-12-13 02:41:38 +0300

    bug fixes + locale

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -872,13 +872,15 @@ namespace Content.Client.Lobby.UI
             var protoId = _historicalUiStorage!.HistoryByButtonId[buttonId];
             var proto = _prototypeManager.Index(protoId);
             _historicalUiStorage!.SelectedHistories[(HistoryType)proto.HistoryType] = protoId;
+            _historicalUiStorage!.OptionButtonsByType[(HistoryType)proto.HistoryType].SelectId(buttonId);
         }
         private void RefreshHistoricalDescriptions()
         {
             foreach (var (key, value) in _historicalUiStorage!.SelectedHistories)
             {
                 var proto = _prototypeManager.Index(value);
-                _historicalUiStorage.DescriptionFieldForTypes[key].TextRope = new Rope.Leaf(proto.Description);
+                var description = Loc.GetString(proto.Description);
+                _historicalUiStorage.DescriptionFieldForTypes[key].TextRope = new Rope.Leaf(description);
             }
         }
         public void RefreshHistorical()
@@ -906,7 +908,7 @@ namespace Content.Client.Lobby.UI
 
                     var infoDescContainer = new BoxContainer() { Align = AlignMode.Center, Orientation = LayoutOrientation.Horizontal };
 
-                    infoDescContainer.AddChild(new Label() { Text = key.ToString(), HorizontalAlignment = HAlignment.Left });
+                    infoDescContainer.AddChild(new Label() { Text = Loc.GetString($"historical-names-{key.ToString()}"), HorizontalAlignment = HAlignment.Left });
                     infoDescContainer.AddChild(new Control() { HorizontalExpand = true });
 
                     optionButton = new OptionButton() { HorizontalAlignment = HAlignment.Right };
@@ -926,7 +928,7 @@ namespace Content.Client.Lobby.UI
                         VerticalExpand = true,
                         HScrollEnabled = false,
                         VScrollEnabled = true,
-                        MinSize = new Vector2(0, 60)
+                        MinSize = new Vector2(0, 120)
                     };
 
                     var descriptionField = new TextEdit
@@ -947,7 +949,8 @@ namespace Content.Client.Lobby.UI
 
                 foreach (var protoId in value)
                 {
-                    optionButton?.AddItem(protoId, lastButtonId++);
+                    var proto = _prototypeManager.Index(protoId);
+                    optionButton?.AddItem(Loc.GetString(proto.Name), lastButtonId++);
                     historyByButtonId.Add(protoId);
                 }
             }
---
+++ Resources/Locale/ru-RU/aws/historical/names.ftl
@@ -0,0 +1,3 @@
+historical-names-Culture = Культура
+historical-names-Lifestyle = Стиль жизни
+historical-names-Faction = Фракция
